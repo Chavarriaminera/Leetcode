e969349228647e8ed1413708700e74d1b5593f18
Cindy <Chavarriaminera@gmail.com>
2026-02-22 19:57:13 -0700

    Time: 263 ms (91.18%), Space: 67.2 MB (31.53%) - LeetHub

--- 2880-select-data/2880-select-data.py
+++ 2880-select-data/2880-select-data.py
@@ -1,8 +1,18 @@
-import pandas as pd
+import pandas as pd 
 
 def selectData(students:pd.DataFrame)->pd.DataFrame:
-    return students.loc[students["student_id"]==101,["name","age"]]
+    return students.loc[students["student_id"]==101, ["name", "age"]]
+
+
+
+
+
+
+    
+
+
     
     
+
 
 
